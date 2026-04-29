67ae4581d75451ca19feb87983c4a71a5908ca2d
dudu <davidionut.miron@gmail.com>
2026-04-29 12:47:22 +0300

    feat: setup Pundit policies and rescue unauthorized errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    render json: { error: "You are not authorized to perform this action." }, status: :forbidden
+  end
 end
