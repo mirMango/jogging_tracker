ed0d92dc9e65ceb0ffb021d38ad6e4deb2e22c12
dudu <davidionut.miron@gmail.com>
2026-04-29 13:14:17 +0300

    feat: implement date filtering and weekly report

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,11 @@ Rails.application.routes.draw do
     defaults: { format: :json }
 
   # Generates all standard REST API routes for jog entries
-  resources :jog_entries
+  resources :jog_entries do
+    collection do
+      get :report
+    end
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
